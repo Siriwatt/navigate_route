1303d5fcf3a4689de0af42b0632e629a3ec56767
siriwatt <siriwatc66@nu.ac.th>
2025-09-04 16:52:11 +0700

    first commit

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:navigate_route/screens/detail_screen.dart';
+
+class HomeScreen extends StatelessWidget {
+  static String routeName = '/';
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Home Screen')),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () async {
+            final result = await Navigator.pushNamed(
+              context,
+              DetailScreen.routeName,
+              arguments: {
+                'itemId': 'ARG-789',
+                'massage': 'ข้อมูลจาก argument',
+              },
+            );
+            print('Returns results: $result');
+          },
+          child: const Text('Go to Detail Screen'),
+        ),
+      ),
+    );
+  }
+}
